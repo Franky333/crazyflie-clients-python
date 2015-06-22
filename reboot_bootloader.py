26133aa26d9e52fad6b56f75abb958f011aefef9
Franky333 <1.daniel.frank@gmail.com>
2015-06-23 00:00:38 +0200

    added shebang to reboot bootloader script

--- reboot_bootloader.py
+++ reboot_bootloader.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #from http://forum.bitcraze.se/viewtopic.php?f=9&t=1488
 
 
@@ -13,9 +14,10 @@ cr = Crazyradio()
 cr.set_channel(27) #change if needed
 cr.set_data_rate(cr.DR_2MPS) #change if needed
 
+
 if cr.send_packet((0xff, 0xfe, 0xff)).ack:   # Init the reboot
 	if cr.send_packet((0xff, 0xfe, 0xf0, 0)).ack:   # Reboot to Bootloader
-#	if cr.send_packet((0xff, 0xfe, 0xf0, 1)).ack:   # Reboot to Firmware
+#	if cr.send_packet((0xff, 0xfe, 0xf0, 1)):   # Reboot to Firmware
 		print "Reboot to Bootloader sucessfull!"
 	else:
 		print "Reboot to Bootloader failed!\nPower CF on and check connection settings."
